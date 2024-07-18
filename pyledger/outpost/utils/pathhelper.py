3e9025067a12fe4884cd8e5ad0031255fb1da216
Florent Valette <florent.valette@ledger.fr>
2024-07-18 16:23:31 +0200

    pathhelper: Add serialization and private builddir in Project path
    rewrite project path using frozen, kw_only dataclass in order to ease
    serialisation/deserialization to/from json.
    Each property is cached (compute only once).
    Python built-in cached-property are not used as R/O properties is
    not allowed with cached-property decorator (need explicit lru_cache
    *and* property decorator w/o setter).
    
    The only meaningfull data to serialize are `project_dir`, `build_dir`
    and `prefix`. All other directory in project layout are deduced from
    those three. Thus, those are exclude from dataclass fields definition
    and forged at runtime (only once due to cache policy).

--- pyledger/outpost/utils/pathhelper.py
+++ pyledger/outpost/utils/pathhelper.py
@@ -2,121 +2,187 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-"""Outpost utils path helper module
-
-This module provides a path helper class PathHelper in order to manipulate
-Outpost project pat and directories.
-"""
-
-import os
+from dataclasses import dataclass, field, asdict
+from enum import unique, auto
+from functools import lru_cache
+import json
 from pathlib import Path
-import typing as T
+from typing import ClassVar
+
+from . import StrEnum
+from ..console import console
+
+@unique
+class DirName(StrEnum):
+    Bin = auto()
+    Build = auto()
+    Configs = auto()
+    Dts = auto()
+    Host = auto()
+    Images = auto()
+    Include = auto()
+    Lib = auto()
+    Man = auto()
+    Outpost_Private = auto()
+    PkgConfig = auto()
+    Share = auto()
+    Src = auto()
+    Sysroot = auto()
+    Target = auto()
 
 
-class ProjectPathHelper:
-    def __init__(self, projectdir: os.PathLike, prefix: T.Optional[os.PathLike] = None) -> None:
-        """Constructor
+def default_prefix() -> Path:
+    """Return default prefix according to build platform.
 
-        :param projectdir: Top level project directory
-        :type projectdir: os.PathLike
-        :param prefix: Install staging prefix, default to None
-        :type prefix: os.PathLike, optional
+    Returns
+    -------
+    Path
+        Build machine specific default prefix path.
+        `C:\\` for Windows (except WSL) build machine
+        `/usr/local` for posix like (any linuxes, WSL, macOS) build machine
 
-        :raises FileNotFoundError: If projectdir does not exist and/or is not found
+    Notes
+    -----
+        This path is absolute
+    """
+    import os
+    if os.name == "nt":
+        return Path("C:/")
+    else:
+        return Path("/usr/local")
 
-        Convert argument projectdir to canonical path
-        """
-        self._projectdir: Path = Path(projectdir).resolve(strict=True)
-        self._prefix: Path = Path(prefix) if prefix is not None else Path("usr", "local")
-        # XXX: use "output" here
-        self._outputdir: Path = Path(self._projectdir, ".")
-        self._sourcedir: Path = Path(self._outputdir, "src")
-        self._builddir: Path = Path(self._outputdir, "build")
-        self._stagingdir: Path = Path(self._outputdir, "staging")
-        self._imagedir: Path = Path(self._outputdir, "image")
-        self._downloaddir: Path = Path(self._outputdir, "dl")
 
-    def create_outputdir_layout(self) -> None:
-        self._outputdir.mkdir(parents=True, exist_ok=True)
-        self._sourcedir.mkdir(parents=True, exist_ok=True)
-        self._builddir.mkdir(parents=True, exist_ok=True)
-        self._stagingdir.mkdir(parents=True, exist_ok=True)
-        self._imagedir.mkdir(parents=True, exist_ok=True)
-        self._downloaddir.mkdir(parents=True, exist_ok=True)
+@dataclass(kw_only=True, frozen=True)
+class ProjectPath:
+    project_dir: Path
+    output_dir: Path
+    prefix: Path = field(default_factory=default_prefix)
 
-    @property
-    def projectdir(self) -> Path:
-        """Project top level directory
-        This directory must exist (created at setup)
-        """
-        return self._projectdir.resolve(strict=True)
+    # ClassVar annotation is used by dataclass decorator in order to exclude member
+    # from consideration as a field and to be ignored by the dataclass mechanisms.
+    filename: ClassVar[str] = "project_tree.json"
+    config_filename: ClassVar[str] = "project.toml"
 
-    @property
-    def outputdir(self) -> Path:
-        """Project output directory
-        This directory must exist (created at setup)
-        """
-        return self._outputdir.resolve(strict=True)
+    def __post_init__(self) -> None:
+        if not self.prefix.is_absolute():
+            # XXX: TODO use dedicated exception
+            raise ValueError("prefix must be an absolute path")
 
-    @property
-    def sourcedir(self) -> Path:
-        """Project source directory
-        This directory must exist (created at setup)
-        """
-        return self._sourcedir.resolve(strict=True)
+        # Convert projectdir and outputdir to canonical path
+        #  projectdir **must** exists
+        #  outputdir **may** not had been created yet
 
-    @property
-    def builddir(self) -> Path:
-        """Project build directory
-        This directory must exist (created at setup)
-        """
-        return self._builddir.resolve(strict=True)
+        # XXX:
+        #  As the dataclass is frozen, `__setattr__` method is deleted from generated class.
+        #  One needs to use object base class `__setattr__` to (post)initialize the instance.
+        #  This is only allowed in `__init__` and `__post_init__` methods.
+        object.__setattr__(self, "project_dir", self.project_dir.resolve(strict=True))
+        object.__setattr__(self, "output_dir", self.output_dir.resolve(strict=False))
+        # XXX: check if config filename, configs and dts dir exist
 
-    @property
-    def stagingdir(self) -> Path:
-        """Project staging directory
-        This directory must exist (created at setup)
-        """
-        return self._stagingdir.resolve(strict=True)
+    @staticmethod
+    def asdict_factory(keyvals) -> dict:
+        return {k: str(v) for k, v in keyvals}
 
-    @property
-    def imagedir(self) -> Path:
-        """Project image directory
-        This directory must exist (created at setup)
-        """
-        return self._imagedir.resolve(strict=True)
+    @classmethod
+    def from_dict(cls, keyvals: dict) -> "ProjectPath":
+        kwargs = {k: Path(v) for k, v in keyvals.items()}
+        return cls(**kwargs)
 
-    @property
-    def downloaddir(self) -> Path:
-        """Project download directory
-        This directory must exist (created at setup)
-        """
-        return self._downloaddir.resolve(strict=True)
+    def save(self) -> None:
+        """Save project path as a json file in project private build dir."""
+        data = asdict(self, dict_factory=self.asdict_factory)
+        with self.save_full_path.open("w") as f:
+            json.dump(data, f, indent=4)
 
-    @property
-    def bindir(self) -> Path:
-        """staging bin directory
-        This directory may not exist yet (created at build time)
-        """
-        return Path(self.stagingdir, self._prefix, "bin").resolve()
-
-    @property
-    def libdir(self) -> Path:
-        """staging lib directory
-        This directory may not exist yet (created at build time)
-        """
-        return Path(self.stagingdir, self._prefix, "lib").resolve()
-
-    @property
-    def datadir(self) -> Path:
-        """staging data directory
-        This directory may not exist yet (created at build time)
-        """
-        return Path(self.stagingdir, self._prefix, "share").resolve()
+    @classmethod
+    def load(cls, build_dir: Path) -> "ProjectPath":
+        """Load project path for a given builddir from json"""
+        try:
+            file = (build_dir / DirName.Outpost_Private.value /cls.filename).resolve(strict=True)
+        except FileNotFoundError as e:
+            # XXX: dedicated error
+            console.critical(f"{cls.filename} not found, please try to re run [i]setup[/i] command")
+            raise
+        with file.open('r') as f:
+            data = json.load(f)
+            return cls.from_dict(data)
 
     @property
-    def pkgconfigdir(self) -> Path:
-        """staging pkgconfig directory
-        This directory may not exist yet (created at build time)
-        """
-        return Path(self.libdir, "pkgconfig").resolve()
+    @lru_cache
+    def config_full_path(self) -> Path:
+        return self.project_dir / self.config_filename
+
+    @property
+    @lru_cache
+    def save_full_path(self) -> Path:
+        return self.private_build_dir / self.filename
+
+    @property
+    @lru_cache
+    def configs_dir(self) -> Path:
+        return self.project_dir / DirName.Configs.value
+
+    @property
+    @lru_cache
+    def dts_dir(self) -> Path:
+        return self.project_dir / DirName.Dts.value
+
+    @property
+    @lru_cache
+    def rel_prefix(self) -> Path:
+        return Path(*self.prefix.parts[1:])
+
+    @property
+    @lru_cache
+    def build_dir(self) -> Path:
+        return self.output_dir / DirName.Build.value
+
+    @property
+    @lru_cache
+    def src_dir(self) -> Path:
+        return self.output_dir / DirName.Src.value
+
+    @property
+    @lru_cache
+    def host_dir(self) -> Path:
+        return self.output_dir / DirName.Host.value
+
+    @property
+    @lru_cache
+    def target_dir(self) -> Path:
+        return self.output_dir / DirName.Target.value
+
+    @property
+    @lru_cache
+    def sysroot_dir(self) -> Path:
+        return self.output_dir / DirName.Sysroot.value
+
+    @property
+    @lru_cache
+    def staging_dir(self) -> Path:
+        # XXX: to be removed
+        return self.output_dir / "staging"
+
+    @property
+    @lru_cache
+    def images_dir(self) -> Path:
+        return self.output_dir / DirName.Images.value
+
+    @property
+    @lru_cache
+    def private_build_dir(self) -> Path:
+        return self.build_dir / DirName.Outpost_Private.value
+
+    def mkdirs(self, exist_ok: bool = True) -> None:
+        self.output_dir.mkdir(parents=True, exist_ok=exist_ok)
+        self.build_dir.mkdir(exist_ok=exist_ok)
+        self.src_dir.mkdir(exist_ok=exist_ok)
+        self.host_dir.mkdir(exist_ok=exist_ok)
+        self.target_dir.mkdir(exist_ok=exist_ok)
+        self.sysroot_dir.mkdir(exist_ok=exist_ok)
+        # XXX: staging to be removed
+        #  need to separate host tools from target devel (sdk/sysroot) and target install
+        self.staging_dir.mkdir(exist_ok=exist_ok)
+        self.images_dir.mkdir(exist_ok=exist_ok)
+        self.private_build_dir.mkdir(exist_ok=exist_ok)
